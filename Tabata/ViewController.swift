f03572ac22a5fc8b870d28fa498eb82d8869db17
Kirill Ki <kirillkalapov@gmail.com>
2021-06-16 01:26:27 +0200

    Well done!

--- Tabata/ViewController.swift
+++ Tabata/ViewController.swift
@@ -6,11 +6,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     var seconds = 30
     var timer = Timer()
+    var player: AVAudioPlayer?
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
     
     
     @IBOutlet weak var label: UILabel!
@@ -59,7 +84,7 @@ class ViewController: UIViewController {
             } else {
              timer.invalidate()
              
-            // playSound()
+            playSound()
             }
         
         
