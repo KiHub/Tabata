a41fb52c74facbfaa22dbc1015e3b2ab5a22e057
Kirill Ki <kirillkalapov@gmail.com>
2021-06-16 01:06:39 +0200

    Working!

--- Tabata/ViewController.swift
+++ Tabata/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var seconds = 30
+    var timer = Timer()
+    
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var slider: UISlider!
@@ -20,9 +23,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        label.text = "30"
         start.layer.cornerRadius = 25
         stop.layer.cornerRadius = 25
         slider.minimumTrackTintColor = UIColor.black
+        //slider.currentMinimumTrackImage = UIImage.
+        slider.setValue(30, animated: true)
         
         
         
@@ -32,12 +38,56 @@ class ViewController: UIViewController {
     
     
     @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        seconds = Int(sender.value)
+        label.text = String(seconds)
+        
     }
     
     @IBAction func startPressed(_ sender: UIButton) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+     
+        @objc func updateTimer() {
+            if seconds != 0 {
+             seconds -= 1
+                label.text = String(seconds)
+                slider.setValue(Float(seconds), animated: true)
+         //    print(Float(secondsPassed) / Float(totalTime))
+                
+            } else {
+             timer.invalidate()
+             
+            // playSound()
+            }
+        
+        
+        
+        
+        
+//
+//        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(counter), userInfo: nil, repeats: true)
+//
+//             func counter() {
+//            seconds -= 1
+//            label.text = String(seconds)
+//            if seconds == 0 {
+//                timer.invalidate()
+//            }
+//
+//
+//        }
+        
+        
     }
     
     @IBAction func stopPressed(_ sender: UIButton) {
+        timer.invalidate()
+        seconds = 30
+        slider.setValue(30, animated: true)
+        label.text = "30"
+        
     }
 }
 
