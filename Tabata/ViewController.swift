a7b7f404a9a8be9c1bc7d0776bcdf763e8b650be
Kirill Ki <kirillkalapov@gmail.com>
2021-06-16 14:45:06 +0200

    Updated with segmented control

--- Tabata/ViewController.swift
+++ Tabata/ViewController.swift
@@ -54,20 +54,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var start: UIButton!
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         label.text = "150"
-        start.layer.cornerRadius = 25
+       // start.layer.cornerRadius = 25
         //   stop.layer.cornerRadius = 25
         slider.minimumTrackTintColor = #colorLiteral(red: 0.337254902, green: 0.2901960784, blue: 0.2901960784, alpha: 1)
         //slider.currentMinimumTrackImage = UIImage.
         slider.setValue(150, animated: true)
         slider.thumbTintColor = #colorLiteral(red: 0.9607843137, green: 0.3607843137, blue: 0.2784313725, alpha: 1)
         
+        //segmentedControl.tintColor = .black //background
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(red: 0.62, green: 0.90, blue: 0.63, alpha: 1.00) ], for: .selected)
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(red: 0.34, green: 0.29, blue: 0.29, alpha: 1.00)], for: .normal)
+        
     }
+    
+    @IBAction func segmentedControlTapped(_ sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 1 :
+            timer.invalidate()
+            seconds = 150
+            slider.setValue(150, animated: true)
+            label.text = "150"
+            
+        case 0 :
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            
+        default:
+            return
+        }
+        
+    }
+    
     
     @IBAction func sliderChanged(_ sender: UISlider) {
         
@@ -117,6 +141,7 @@ class ViewController: UIViewController {
         } else {
             timer.invalidate()
             playSoundTwice()
+            segmentedControl.selectedSegmentIndex = UISegmentedControl.noSegment
             
         }
     }
